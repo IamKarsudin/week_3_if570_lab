9a9534634f3b07b595502affc6f238fbaf78aac6
Karsudin <karsudin@Student.umn.ac.id>
2023-03-01 11:47:00 +0700

    Comparing abstract classes and interface

--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -26,6 +26,17 @@ fun buildAquarium() {
     myTower.printSize()
 }
 
-fun main() {
-    buildAquarium()
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+    println("Shark: ${shark.color}")
+    shark.eat()
+    println("Plecostomus: ${pleco.color}")
+    pleco.eat()
 }
+
+fun main() {
+    /** buildAquarium() **/
+    makeFish()
+}
+
